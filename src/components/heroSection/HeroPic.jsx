b5619a95dcba96c3d97e1583c6bd505ff10a540c
Akila Liyanage <it21180484@my.sliit.lk>
2025-07-04 03:58:31 +0530

    pic update

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/AkilaLiyanage.png"
+        src="https://github.com/LAIkumara/my-portfolio/blob/main/public/images/AkilaLiyanage.png?raw=true"
         alt="Abdelrahman Qassem"
         className="max-h-[450px] w-auto"
       />
