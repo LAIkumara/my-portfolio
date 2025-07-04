90fbe736ea897a2b4b7fac7cc795a418181cf1e9
Akila Liyanage <it21180484@my.sliit.lk>
2025-07-04 14:18:54 +0530

    update details

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,11 +1,11 @@
 const NavbarLogo = () => {
   return (
     <div>
-      <h1 className="text-white  text-2xl sm:hidden md:block">
+      <h1 className="navbar-logo text-white  text-2xl">
         Akila Liyanage
       </h1>
-      <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        AQ
+      <h1 className="navbar-logo2 text-white font-special font-extrabold text-4xl md:hidden sm:block">
+        AL
       </h1>
     </div>
   );
