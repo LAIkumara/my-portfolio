36a394f56eefb1066272e4fa45a144aad7a05f1c
Liyanage Kumara <90019916+LAIkumara@users.noreply.github.com>
2025-09-09 09:41:01 +0530

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="https://github.com/LAIkumara/my-portfolio/blob/main/public/images/AkilaLiyanage.png?raw=true"
+        src="https://github.com/LAIkumara/my-portfolio/blob/main/public/images/AkilaLiyanage.png"
         alt="Abdelrahman Qassem"
         className="max-h-[450px] w-auto"
       />
